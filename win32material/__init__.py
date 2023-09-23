c18151a5ba9361fca340df2d34a774a7cb9c6ad3
小白云 <71159641+littlewhitecloud@users.noreply.github.com>
2023-09-23 10:49:33 +0800

    Delete win32material directory

--- win32material/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Load functions from win32material.py"""
-from platform import platform
-
-if not platform().startswith("Windows-10"):
-    raise OSError("To use this package, you need to use Windows version >= 10")
-
-from .win32material import ApplyAcrylic, ApplyBlur, ApplyMica, ChangeTitlebarColor, ChangeBorderColor, ChangeTitleColor, SetBorderType
-
-from .win32material import MICAMODE, MICATHEME, BORDERTYPE
