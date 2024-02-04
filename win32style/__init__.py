cee29abf4f7ed8a1e8dff2a8d8bed61f0982ce54
小白云 <71159641+littlewhitecloud@users.noreply.github.com>
2024-02-04 15:16:19 +0800

    a lot of change
    acrylic can apply to the whole window

--- win32style/__init__.py
+++ win32style/__init__.py
@@ -4,8 +4,15 @@ from platform import platform
 if not platform().startswith("Windows-10"):
     raise OSError("To use this package, you need to use Windows version >= 10")
 
-from .win32material import ApplyAcrylic, ApplyBlur, ApplyMica
-from .win32material import ChangeTitlebarColor, ChangeBorderColor, ChangeTitleColor, \
-                            SetBorderType, ApplyDarkMode
-
-from .win32material import MICAMODE, MICATHEME, BORDERTYPE
+from .win32style import (
+    BORDERTYPE,
+    MICAMODE,
+    MICATHEME,
+    ApplyAcrylic,
+    ApplyDarkMode,
+    ApplyMica,
+    ChangeBorderColor,
+    ChangeTitlebarColor,
+    ChangeTitleColor,
+    SetBorderType,
+)
--- win32style/build.bat
+++ win32style/build.bat
@@ -1,7 +1,5 @@
 echo off
 
 cl /LD /DEF: .\win32style.def .\win32style.cpp
-cl /LD /DEF: .\win32material.def .\win32material.cpp
 
-del win32material.exp win32material.lib win32material.obj
 del win32style.exp win32style.lib win32style.obj
