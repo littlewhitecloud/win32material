e857518d011713199c98004eb755cbe3a88114e7
小白云 <71159641+littlewhitecloud@users.noreply.github.com>
2024-07-01 18:14:55 +0800

    update

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 """Setup for win32material"""
+
 from distutils.core import setup
 
 with open("README.md", "r") as file:
@@ -6,19 +7,11 @@ with open("README.md", "r") as file:
 
 setup(
     name="win32material",
-    version="1.0.5",
+    version="1.0.6",
     description="Apply some window effects to the Win32 Applications",
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="littlewhitecloud",
-    exclude_package_data={
-        "": ["win32material.def"],
-        "": ["win32material.h"],
-        "": ["win32material.cpp"],
-    },
-    package_data={
-        "": ["*.dll"],
-    },
     url="https://github.com/littlewhitecloud/win32material",
     packages=["win32material"],
 )
