d7f99bafb4216f1a1cf237bee01b6e7914db19f8
Zhikang Yan <2951256653@qq.com>
2025-01-06 21:48:21 +0800

    fix: Change the `OSError` to `ImportError` when the runtime platform is not Windows 10 or above

--- win32material/__init__.py
+++ win32material/__init__.py
@@ -3,7 +3,7 @@
 from platform import platform
 
 if not platform().startswith("Windows-10") and not platform().startswith("Windows-11"):
-    raise OSError("To use this package, you need to use Windows 10 or above")
+    raise ImportError("To use this package, you need to use Windows 10 or above")
 
 from .win32material import (BORDERTYPE, MICAMODE, MICATHEME, ApplyAcrylic,
                             ApplyDarkMode, ApplyMica, SetBorderColor,
